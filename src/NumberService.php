f586bc820239317be25f3e1d7859b20c5c778ab9
hamdon <cao4141@qq.com>
2020-09-09 10:26:59 +0800

    给NumberService类增加过滤时间格式功能

--- src/NumberService.php
+++ src/NumberService.php
@@ -65,4 +65,111 @@ class NumberService
         $willFee = ceil($fee * $powValue);
         return sprintf('%.' . $digits . 'f', $willFee / $powValue);
     }
+
+    /**
+     * 过滤时间格式-调整为正确的时间
+     *
+     * @param $dayTime
+     * @return string
+     */
+    public function dateTimeFilter($dayTime)
+    {
+        if (strlen($dayTime) != 19) {
+            $days = explode(' ', $dayTime);
+            //过滤年月日
+            if (strlen($days[0]) != 10) {
+                $dates = explode('-', $days[0]);
+                //过滤年份
+                if (strlen($dates[0]) != 4) {
+                    if (substr($dates[0], 0, 1) == '2') {
+                        $dates[0] = substr($dates[0], 0, 4);
+                    } else {
+                        $dates[0] = substr($dates[0], -4);
+                    }
+                }
+                //过滤月份
+                if (strlen($dates[1]) != 2) {
+                    $month = intval($dates[1]);
+                    if (strlen($month) != 2) {
+                        if (strlen($month) == 1) {
+                            $dates[1] = '0' . $month;
+                        } else {
+                            $dates[1] = substr($month, 0, 2);
+                        }
+                    } else {
+                        $dates[1] = $month;
+                    }
+                    if ($dates[1] > 12) {
+                        $dates[1] = 12;
+                    }
+                }
+                //过滤天
+                if (strlen($dates[2]) != 2) {
+                    $day = intval($dates[2]);
+                    if (strlen($day) != 2) {
+                        if (strlen($day) == 1) {
+                            $dates[2] = '0' . $day;
+                        } elseif (strlen($day) > 2) {
+                            $dates[2] = substr($day, 0, 2);
+                        } else {
+                            $dates[2] = $day;
+                        }
+                    } else {
+                        $dates[2] = $day;
+                    }
+                    if ($dates[2] > 31) {
+                        $dates[2] = 31;
+                    }
+                }
+                $days[0] = implode('-', $dates);
+            }
+            //过滤时分秒
+            if (strlen($days[1] != 8)) {
+                $times = explode(':', $days[1]);
+                //过滤小时
+                if (strlen($times[0]) != 2) {
+                    $mTime = intval($times[0]);
+                    if (strlen($mTime) > 2) {
+                        $mTime = substr($mTime, 0, 2);
+                    } elseif (strlen($mTime) == 1) {
+                        $mTime = '0' . $mTime;
+                    }
+                    if ($mTime > 23) {
+                        $mTime = 23;
+                    }
+                    $times[0] = $mTime;
+                }
+                //过滤分钟
+                if (strlen($times[1]) != 2) {
+                    $hTime = intval($times[1]);
+                    if (strlen($hTime) > 2) {
+                        $hTime = substr($hTime, 0, 2);
+                    } elseif (strlen($hTime) == 1) {
+                        $hTime = '0' . $hTime;
+                    }
+                    if ($hTime > 59) {
+                        $hTime = 59;
+                    }
+                    $times[1] = $hTime;
+                }
+                //过滤秒
+                if (strlen($times[2]) != 2) {
+                    $sTime = intval($times[2]);
+                    if (strlen($sTime) > 2) {
+                        $sTime = substr($sTime, 0, 2);
+                    } elseif (strlen($sTime) == 1) {
+                        $sTime = '0' . $sTime;
+                    }
+                    if ($sTime > 59) {
+                        $sTime = 59;
+                    }
+                    $times[2] = $sTime;
+                }
+                $days[1] = implode(':', $times);
+            }
+            //整合
+            $dayTime = implode(' ', $days);
+        }
+        return $dayTime;
+    }
 }
